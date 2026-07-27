56a0d9d338b1fa37dd6a0bd080ed1c2f3e7c4068
tsiksdev-sketch <tsiksdev@gmail.com>
2026-07-27 16:15:18 +0200

    #

---
+++ app/for-professionals/proffesional-documents/page.tsx
@@ -0,0 +1,22 @@
+import { ContentPage } from "@/components/contentPage";
+import { pageContents } from "@/constants/pages";
+import type { Metadata } from "next";
+
+const page = pageContents["for-professionals__proffesional-documents"];
+
+export const metadata: Metadata = {
+  title: page.seoTitle,
+  description: page.metaDescription,
+  alternates: {
+    canonical: page.url,
+  },
+  openGraph: {
+    title: page.seoTitle,
+    description: page.metaDescription,
+    type: "article",
+  },
+};
+
+export default function Page() {
+  return <ContentPage page={page} />;
+}
